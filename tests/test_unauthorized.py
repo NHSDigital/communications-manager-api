1ada3e4d798379753176853d529707422ad3a259
Rachel Kennedy <rachel.kennedy@test-and-trace.nhs.uk>
2023-07-06 13:45:24 +0100

    CCM-924: Add invalid auth prod tests
    CCM-924

--- tests/test_unauthorized.py
+++ tests/test_unauthorized.py
@@ -8,9 +8,10 @@ Scenarios
 import requests
 import pytest
 
-INVALID_TOKEN = {
+MOCK_TOKEN = {
     "Authorization": "Bearer InvalidMockToken"
 }
+INVALID_TOKEN = ["Bearer xyzcba", "Bearer", "junk"]
 
 
 def __assert_401_error(resp, check_body=True):
@@ -29,43 +30,43 @@ def __assert_401_error(resp, check_body=True):
 
 @pytest.mark.sandboxtest
 def test_401_invalid_sandbox_get(nhsd_apim_proxy_url):
-    resp = requests.get(f"{nhsd_apim_proxy_url}", headers=INVALID_TOKEN)
+    resp = requests.get(f"{nhsd_apim_proxy_url}", headers=MOCK_TOKEN)
     __assert_401_error(resp)
 
 
 @pytest.mark.sandboxtest
 def test_401_invalid_sandbox_post(nhsd_apim_proxy_url):
-    resp = requests.post(f"{nhsd_apim_proxy_url}", headers=INVALID_TOKEN)
+    resp = requests.post(f"{nhsd_apim_proxy_url}", headers=MOCK_TOKEN)
     __assert_401_error(resp)
 
 
 @pytest.mark.sandboxtest
 def test_401_invalid_sandbox_put(nhsd_apim_proxy_url):
-    resp = requests.put(f"{nhsd_apim_proxy_url}", headers=INVALID_TOKEN)
+    resp = requests.put(f"{nhsd_apim_proxy_url}", headers=MOCK_TOKEN)
     __assert_401_error(resp)
 
 
 @pytest.mark.sandboxtest
 def test_401_invalid_sandbox_patch(nhsd_apim_proxy_url):
-    resp = requests.patch(f"{nhsd_apim_proxy_url}", headers=INVALID_TOKEN)
+    resp = requests.patch(f"{nhsd_apim_proxy_url}", headers=MOCK_TOKEN)
     __assert_401_error(resp)
 
 
 @pytest.mark.sandboxtest
 def test_401_invalid_sandbox_delete(nhsd_apim_proxy_url):
-    resp = requests.delete(f"{nhsd_apim_proxy_url}", headers=INVALID_TOKEN)
+    resp = requests.delete(f"{nhsd_apim_proxy_url}", headers=MOCK_TOKEN)
     __assert_401_error(resp)
 
 
 @pytest.mark.sandboxtest
 def test_401_invalid_sandbox_head(nhsd_apim_proxy_url):
-    resp = requests.head(f"{nhsd_apim_proxy_url}", headers=INVALID_TOKEN)
+    resp = requests.head(f"{nhsd_apim_proxy_url}", headers=MOCK_TOKEN)
     __assert_401_error(resp, check_body=False)
 
 
 @pytest.mark.sandboxtest
 def test_401_invalid_sandbox_options(nhsd_apim_proxy_url):
-    resp = requests.options(f"{nhsd_apim_proxy_url}", headers=INVALID_TOKEN)
+    resp = requests.options(f"{nhsd_apim_proxy_url}", headers=MOCK_TOKEN)
     __assert_401_error(resp)
 
 
@@ -152,3 +153,52 @@ def test_401_missing_delete(nhsd_apim_proxy_url):
 def test_401_missing_options(nhsd_apim_proxy_url):
     resp = requests.options(f"{nhsd_apim_proxy_url}")
     __assert_401_error(resp, check_body=False)
+
+
+@pytest.mark.prodtest
+@pytest.mark.parametrize('invalid_token', INVALID_TOKEN)
+def test_401_invalid_prod_get(nhsd_apim_proxy_url, invalid_token):
+    resp = requests.get(f"{nhsd_apim_proxy_url}", headers={"Authorization": invalid_token})
+    __assert_401_error(resp)
+
+
+@pytest.mark.prodtest
+@pytest.mark.parametrize('invalid_token', INVALID_TOKEN)
+def test_401_invalid_prod_post(nhsd_apim_proxy_url, invalid_token):
+    resp = requests.post(f"{nhsd_apim_proxy_url}", headers={"Authorization": invalid_token})
+    __assert_401_error(resp)
+
+
+@pytest.mark.prodtest
+@pytest.mark.parametrize('invalid_token', INVALID_TOKEN)
+def test_401_invalid_prod_put(nhsd_apim_proxy_url, invalid_token):
+    resp = requests.put(f"{nhsd_apim_proxy_url}", headers={"Authorization": invalid_token})
+    __assert_401_error(resp)
+
+
+@pytest.mark.prodtest
+@pytest.mark.parametrize('invalid_token', INVALID_TOKEN)
+def test_401_invalid_prod_patch(nhsd_apim_proxy_url, invalid_token):
+    resp = requests.patch(f"{nhsd_apim_proxy_url}", headers={"Authorization": invalid_token})
+    __assert_401_error(resp)
+
+
+@pytest.mark.prodtest
+@pytest.mark.parametrize('invalid_token', INVALID_TOKEN)
+def test_401_invalid_prod_delete(nhsd_apim_proxy_url, invalid_token):
+    resp = requests.delete(f"{nhsd_apim_proxy_url}", headers={"Authorization": invalid_token})
+    __assert_401_error(resp)
+
+
+@pytest.mark.prodtest
+@pytest.mark.parametrize('invalid_token', INVALID_TOKEN)
+def test_401_invalid_prod_head(nhsd_apim_proxy_url, invalid_token):
+    resp = requests.head(f"{nhsd_apim_proxy_url}", headers={"Authorization": invalid_token})
+    __assert_401_error(resp, check_body=False)
+
+
+@pytest.mark.prodtest
+@pytest.mark.parametrize('invalid_token', INVALID_TOKEN)
+def test_401_invalid_prod_options(nhsd_apim_proxy_url, invalid_token):
+    resp = requests.options(f"{nhsd_apim_proxy_url}", headers={"Authorization": invalid_token})
+    __assert_401_error(resp)
