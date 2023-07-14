e9c25969a6ce56b3f60bd11fc851cf21d4f55174
Rachel Kennedy <rachel.kennedy@test-and-trace.nhs.uk>
2023-07-14 15:27:34 +0100

    CCM-1065: test fix

--- tests/lib/generators.py
+++ tests/lib/generators.py
@@ -4,94 +4,114 @@ from .constants import *
 class Generators():
     @staticmethod
     def generate_invalid_value_error(pointer):
-        return Generators.generate_error(ERROR_INVALID_VALUE, {
+        return Generators.generate_error(ERROR_INVALID_VALUE, source={
             "pointer": pointer
         })
 
     @staticmethod
     def generate_missing_value_error(pointer):
-        return Generators.generate_error(ERROR_MISSING_VALUE, {
+        return Generators.generate_error(ERROR_MISSING_VALUE, source={
             "pointer": pointer
         })
 
     @staticmethod
     def generate_null_value_error(pointer):
-        return Generators.generate_error(ERROR_NULL_VALUE, {
+        return Generators.generate_error(ERROR_NULL_VALUE, source={
             "pointer": pointer
         })
 
     @staticmethod
     def generate_duplicate_value_error(pointer):
-        return Generators.generate_error(ERROR_DUPLICATE_VALUE, {
+        return Generators.generate_error(ERROR_DUPLICATE_VALUE, source={
             "pointer": pointer
         })
 
     @staticmethod
     def generate_too_few_items_error(pointer):
-        return Generators.generate_error(ERROR_TOO_FEW_ITEMS, {
+        return Generators.generate_error(ERROR_TOO_FEW_ITEMS, source={
             "pointer": pointer
         })
 
     @staticmethod
     def generate_access_denied_error():
-        return Generators.generate_error(ERROR_ACCESS_DENIED, {
+        return Generators.generate_error(ERROR_ACCESS_DENIED, source={
             "header": "Authorization"
         })
 
     @staticmethod
     def generate_forbidden_error():
-        return Generators.generate_error(ERROR_FORBIDDEN, {
+        return Generators.generate_error(ERROR_FORBIDDEN, source={
             "header": "Authorization"
         })
 
     @staticmethod
     def generate_not_acceptable_error():
-        return Generators.generate_error(ERROR_NOT_ACCEPTABLE, {
+        return Generators.generate_error(ERROR_NOT_ACCEPTABLE, source={
             "header": "Accept"
         })
 
     @staticmethod
     def generate_unsupported_media_error():
-        return Generators.generate_error(ERROR_UNSUPPORTED_MEDIA, {
+        return Generators.generate_error(ERROR_UNSUPPORTED_MEDIA, source={
             "header": "Content-Type"
         })
 
     @staticmethod
     def generate_no_such_routing_plan_error():
-        return Generators.generate_error(ERROR_NO_SUCH_ROUTING_PLAN, {
+        return Generators.generate_error(ERROR_NO_SUCH_ROUTING_PLAN, source={
             "pointer": "/data/attributes/routingPlanId"
         })
 
     @staticmethod
     def generate_missing_routing_plan_template_error():
-        return Generators.generate_error(ERROR_MISSING_ROUTING_PLAN_TEMPLATE, {
+        return Generators.generate_error(ERROR_MISSING_ROUTING_PLAN_TEMPLATE, source={
             "pointer": "/data/attributes/routingPlanId"
         })
 
     @staticmethod
     def generate_duplicate_routing_plan_template_error():
-        return Generators.generate_error(ERROR_DUPLICATE_ROUTING_PLAN_TEMPLATE, {
+        return Generators.generate_error(ERROR_DUPLICATE_ROUTING_PLAN_TEMPLATE, source={
             "pointer": "/data/attributes/routingPlanId"
-        })
+        }, meta={"duplicateTemplates" : [
+            {
+                "name": "EMAIL_TEMPLATE",
+                "type": "EMAIL"
+            },
+            {
+                "name": "SMS_TEMPLATE",
+                "type": "SMS"
+            },{
+                "name": "LETTER_TEMPLATE",
+                "type": "LETTER"
+            },
+            {
+                "name": "LETTER_PDF_TEMPLATE",
+                "type": "LETTER_PDF"
+            },
+            {
+                "name": "NHSAPP_TEMPLATE",
+                "type": "NHSAPP"
+            }
+        ]})
 
     @staticmethod
     def generate_not_found_error():
-        return Generators.generate_error(ERROR_NOT_FOUND, None)
+        return Generators.generate_error(ERROR_NOT_FOUND)
 
     @staticmethod
     def generate_quota_error():
-        return Generators.generate_error(ERROR_QUOTA, None)
+        return Generators.generate_error(ERROR_QUOTA)
 
     @staticmethod
     def generate_request_timeout_error():
-        return Generators.generate_error(ERROR_REQUEST_TIMEOUT, None)
+        return Generators.generate_error(ERROR_REQUEST_TIMEOUT)
 
     @staticmethod
     def generate_service_timeout_error():
-        return Generators.generate_error(ERROR_SERVICE_TIMEOUT, None)
+        return Generators.generate_error(ERROR_SERVICE_TIMEOUT)
 
     @staticmethod
-    def generate_error(error, source):
+    def generate_error(error, source=None, meta=None):
         ret = {
             "id": error.id,
             "links": error.links,
@@ -102,5 +122,7 @@ class Generators():
 
         if source:
             ret["source"] = source
-
+            
+        if meta:
+            ret["meta"] = meta
         return ret
