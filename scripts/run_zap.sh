42a2304a73b08f147d2b47487e8b49a63fa65173
Philip Skinner <philip.skinner@bjss.com>
2023-08-04 16:04:14 +0100

    CCM-1267
    Adds zap security scanner to the nightly test runs.
    
    Tidies up the specification to remove invalid server addresses.
    
    Skips the proxy check if on the nightly test run.
    
    Removes run-postman.js script as it is not used.

---
+++ scripts/run_zap.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -o nounset errexit pipefail
+
+# create our temporary directory
+export TEMP_DIR=/tmp
+
+echo "Storing reports in $TEMP_DIR"
+
+# make sure we have published the spec
+npm run publish
+
+# run zap in a container
+docker container run \
+    -v $(pwd):/zap/wrk/:rw \
+    -v $TEMP_DIR:/zap/tmp/:rw \
+    -t softwaresecurityproject/zap-stable bash \
+    -c "./zap.sh -addoninstall openapi -cmd -autorun /zap/wrk/zap/zap.yaml"
+
+# generate our nunit report from the zap JSON report
+./node_modules/.bin/hbs --data $TEMP_DIR/zap-report.json zap/nunit-template.hbs -s > zap-report.xml
+
+# copy all reporting files over for publishing
+cp $TEMP_DIR/zap-report.html .
