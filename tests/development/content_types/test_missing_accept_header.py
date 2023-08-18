94322d8c891c0a87ef2f706360d341995596d2a8
Sean Marjason <sean.marjason@bjss.com>
2023-08-18 16:28:31 +0100

    CCM-1405: Fix test headers

--- tests/development/content_types/test_missing_accept_header.py
+++ tests/development/content_types/test_missing_accept_header.py
@@ -2,29 +2,32 @@ import requests
 import pytest
 from lib import Assertions, Generators
 
-REQUEST_PATH = ["/v1/message-batches"]
-METHODS = ["post"]
+REQUEST_PATH = "/v1/message-batches"
 CORRELATION_IDS = [None, "88b10816-5d45-4992-bed0-ea685aaa0e1f"]
+VALID_CONTENT_TYPE_HEADERS = ["application/json", "application/vnd.api+json"]
 
 
 @pytest.mark.devtest
-@pytest.mark.parametrize("request_path", REQUEST_PATH)
 @pytest.mark.parametrize("correlation_id", CORRELATION_IDS)
-@pytest.mark.parametrize("method", METHODS)
+@pytest.mark.parametrize('content_type', VALID_CONTENT_TYPE_HEADERS)
 @pytest.mark.nhsd_apim_authorization({"access": "application", "level": "level3"})
 def test_missing_accept_header(
     nhsd_apim_proxy_url,
-    request_path,
     correlation_id,
-    method,
+    content_type,
     nhsd_apim_auth_headers
 ):
     data = Generators.generate_valid_create_message_batch_body()
 
-    resp = getattr(requests, method)(f"{nhsd_apim_proxy_url}/{request_path}", headers={
-        **nhsd_apim_auth_headers,
-        "X-Correlation-Id": correlation_id
-    })
+    resp = requests.post(
+        f"{nhsd_apim_proxy_url}{REQUEST_PATH}",
+        headers={
+            "Content-Type": content_type,
+            "X-Correlation-Id": correlation_id,
+            **nhsd_apim_auth_headers,
+        },
+        json=data
+    )
 
     if resp.status_code == 429:
         raise AssertionError('Unexpected 429')
