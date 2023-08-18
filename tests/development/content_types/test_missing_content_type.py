4461b764cb57b6adbed3804b4fb2540cf92b9645
Sean Marjason <sean.marjason@bjss.com>
2023-08-18 13:54:46 +0100

    CCM-1405: Allow requests with no Accept Header

--- tests/development/content_types/test_missing_content_type.py
+++ tests/development/content_types/test_missing_content_type.py
@@ -1,9 +1,8 @@
 import requests
 import pytest
-from lib import Assertions, Generators
 
-HEADER_NAME = ["accept", "ACCEPT", "Accept", "AcCePt"]
-HEADER_VALUE = ["application/xml", "image/png", "text/plain", "audio/mpeg", "xyz/abc"]
+HEADER_NAME = ["", "accept", "ACCEPT", "Accept", "AcCePt"]
+HEADER_VALUE = [""]
 REQUEST_PATH = ["/v1/ignore", "/api/ignore"]
 METHODS = ["get", "post", "put", "patch", "delete", "head", "options"]
 CORRELATION_IDS = [None, "88b10816-5d45-4992-bed0-ea685aaa0e1f"]
@@ -16,7 +15,7 @@ CORRELATION_IDS = [None, "88b10816-5d45-4992-bed0-ea685aaa0e1f"]
 @pytest.mark.parametrize("correlation_id", CORRELATION_IDS)
 @pytest.mark.parametrize("method", METHODS)
 @pytest.mark.nhsd_apim_authorization({"access": "application", "level": "level3"})
-def test_406(
+def test_missing_accept_header(
     nhsd_apim_proxy_url,
     accept_header_name,
     accept_header_value,
@@ -31,9 +30,4 @@ def test_406(
         "X-Correlation-Id": correlation_id
     })
 
-    Assertions.assert_error_with_optional_correlation_id(
-        resp,
-        406,
-        Generators.generate_not_acceptable_error() if method not in ["options", "head"] else None,
-        correlation_id
-    )
+    assert resp.status_code == 201
