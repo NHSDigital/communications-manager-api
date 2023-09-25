ef4115bee92188443f9369a4fdf2ba8b429c3cad
Ben <tet@>
2023-09-25 09:26:36 +0100

    locust tests post to vs/message-batches

--- tests/locust/test_quota.py
+++ tests/locust/test_quota.py
@@ -1,10 +1,5 @@
 from locust import HttpUser, task, constant, LoadTestShape, TaskSet
-
-
-class UserTasks(TaskSet):
-    @task
-    def hit_endpoint(self):
-        self.client.get("/_ping")
+from user_tasks import UserTasks
 
 
 class QuotaUser(HttpUser):
