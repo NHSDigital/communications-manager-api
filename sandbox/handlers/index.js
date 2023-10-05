090e54c169f803760694651514ef5239ffb46190
Ben <tet@>
2023-10-05 16:54:49 +0100

    CCM-1652: init

--- sandbox/handlers/index.js
+++ sandbox/handlers/index.js
@@ -2,6 +2,7 @@
 
 const { status } = require("./status");
 const { batch_send } = require("./batch_send");
+const { messages } = require("./messages");
 const { trigger_timeout } = require("./trigger_timeout");
 const { backend_408 } = require("./backend_408");
 const { backend_504 } = require("./backend_504");
@@ -9,6 +10,7 @@ const { backend_504 } = require("./backend_504");
 module.exports = {
     status,
     batch_send,
+    messages,
     trigger_timeout,
     backend_408,
     backend_504
