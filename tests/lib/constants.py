5d920332870448807b992b79b12748e63d036549
Rachel Kennedy <rachel.kennedy13@nhs.net>
2023-10-10 17:19:36 +0100

    ccm-1903: mtls uat test

--- tests/lib/constants.py
+++ tests/lib/constants.py
@@ -19,6 +19,7 @@ NUM_MAX_ERRORS = 100
 DEV_API_GATEWAY_URL = "https://comms-apim.internal-dev.communications.national.nhs.uk"
 INT_API_GATEWAY_URL = "https://comms-apim.int.communications.national.nhs.uk"
 PROD_API_GATEWAY_URL = "https://comms-apim.prod.communications.national.nhs.uk"
+UAT_API_GATEWAY_URL = "https://comms-apim.uat.communications.national.nhs.uk"
 DEFAULT_CONTENT_TYPE = "application/vnd.api+json"
 UNEXPECTED_429 = AssertionError('Unexpected 429')
 UNEXPECTED_504 = AssertionError('Unexpected 504')
