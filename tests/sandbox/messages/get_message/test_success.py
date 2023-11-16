61f60daa1a0b51396ad584f3990d2c821ebe59ea
Ben <tet@>
2023-11-16 16:06:50 +0000

    CCM-1883: parameterise 200 message ids

--- tests/sandbox/messages/get_message/test_success.py
+++ tests/sandbox/messages/get_message/test_success.py
@@ -1,3 +1,4 @@
+import os
 import requests
 import pytest
 from lib import Assertions, Generators
@@ -5,15 +6,22 @@ import lib.constants.constants as constants
 from lib.constants.messages_paths import MESSAGES_ENDPOINT
 
 
+def get_200_message_ids():
+    directory_path = os.path.join(os.path.dirname(__file__), '../../../../sandbox/messages')
+    return [os.path.splitext(file)[0] for file in os.listdir(directory_path)]
+
+
 @pytest.mark.sandboxtest
 @pytest.mark.parametrize('accept_headers', constants.VALID_ACCEPT_HEADERS)
-def test_200_get_message_valid_accept_headers(nhsd_apim_proxy_url, accept_headers):
+@pytest.mark.parametrize('message_ids', get_200_message_ids())
+def test_200_get_message_valid_accept_headers(nhsd_apim_proxy_url, accept_headers, message_ids):
+
     """
     .. include:: ../../partials/happy_path/test_200_messages_message_id.rst
     """
     data = Generators.generate_valid_create_message_body("sandbox")
     resp = requests.get(
-        f"{nhsd_apim_proxy_url}{MESSAGES_ENDPOINT}/2WL4QcKGjNHvHFQeKgYbapZJGHK",
+        f"{nhsd_apim_proxy_url}{MESSAGES_ENDPOINT}/{message_ids}",
         headers={
             "Accept": accept_headers,
             "Content-Type": "application/json"
