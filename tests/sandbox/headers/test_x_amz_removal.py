8ba3f2bc1c2f6dc1c223cbf9fd5dd083b17a5f20
Tom Webb <tom.webb4@nhs.net>
2024-01-03 13:05:08 +0000

    CCM-2152: Standardise use of constant

--- tests/sandbox/headers/test_x_amz_removal.py
+++ tests/sandbox/headers/test_x_amz_removal.py
@@ -2,7 +2,7 @@ import requests
 import pytest
 
 from lib import Assertions
-from lib.constants.constants import VALID_ENDPOINTS, TEST_METHODS
+from lib.constants.constants import VALID_ENDPOINTS, TEST_METHODS, ORIGIN
 
 
 @pytest.mark.sandboxtest
@@ -12,7 +12,7 @@ def test_request_with_x_amz_is_removed(nhsd_apim_proxy_url, endpoints, method):
 
     resp = getattr(requests, method)(f"{nhsd_apim_proxy_url}/{endpoints}", headers={
         "Accept": "*/*",
-        "Origin": "https://my.website"
+        "Origin": ORIGIN
     })
 
     Assertions.assert_no_aws_headers(resp)
