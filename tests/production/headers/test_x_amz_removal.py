4d78e2a95ce915f17550013c83d8a16eb06daca4
Rachel Kennedy <rachel.kennedy13@nhs.net>
2024-01-11 10:38:54 +0000

    CCM-2614: Comment out tests that wont pass until 4.9.0

--- tests/production/headers/test_x_amz_removal.py
+++ tests/production/headers/test_x_amz_removal.py
@@ -5,7 +5,7 @@ from lib import Assertions, Authentication
 from lib.constants.constants import VALID_ENDPOINTS, ORIGIN, METHODS
 
 
-@pytest.mark.prodtest
+# Add prodtest once 4.9.0 is in prod
 @pytest.mark.parametrize("method", METHODS)
 @pytest.mark.parametrize("endpoints", VALID_ENDPOINTS)
 def test_request_with_x_amz_is_removed(nhsd_apim_proxy_url, endpoints, method):
