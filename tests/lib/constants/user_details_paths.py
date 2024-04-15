60d7c8cfff05ba9cbf0d17ce188312a233f60fe3
Simon Labarere <simon.labarere1@nhs.net>
2024-04-15 10:53:13 +0100

    CCM-3898: Page not found test

---
+++ tests/lib/constants/user_details_paths.py
@@ -0,0 +1,15 @@
+USER_DETAILS_ENDPOINT = "/channels/nhsapp/accounts"
+ODS_CODE_PARAM_NAME = "ods-organisation-code"
+PAGE_PARAM_NAME = "page"
+
+
+VALID_ODS_CODES = [
+    "X26",
+    "X00001",
+    "T00001",
+    "X12345"
+]
+
+ODS_CODES_WITH_PAGINATED_RESULTS = [
+    "T00001"
+]
