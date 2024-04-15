f6d032af2a6eea9ec6afbab9f80eec97ed066841
Simon Labarere <simon.labarere1@nhs.net>
2024-04-15 11:12:44 +0100

    CCM-3898: Page not found test

--- tests/lib/constants/user_details_paths.py
+++ tests/lib/constants/user_details_paths.py
@@ -2,13 +2,12 @@ USER_DETAILS_ENDPOINT = "/channels/nhsapp/accounts"
 ODS_CODE_PARAM_NAME = "ods-organisation-code"
 PAGE_PARAM_NAME = "page"
 
-VALID_ODS_CODES = [
+SINGLE_PAGE_ODS_CODES = [
     "X26",
     "X00001",
-    "T00001",
     "X12345"
 ]
 
-ODS_CODES_WITH_PAGINATED_RESULTS = [
+MULTIPLE_PAGES_ODS_CODES = [
     "T00001"
 ]
--- tests/sandbox/user_details/test_404.py
+++ tests/sandbox/user_details/test_404.py
@@ -2,14 +2,14 @@ import requests
 import pytest
 from lib import Assertions, Generators
 from lib.constants.user_details_paths import USER_DETAILS_ENDPOINT, ODS_CODE_PARAM_NAME, PAGE_PARAM_NAME, \
-    VALID_ODS_CODES
+    SINGLE_PAGE_ODS_CODES
 
 INVALID_PAGE_NUMBERS = [2, 3, 4, 5, 6]
 CORRELATION_IDS = [None, "228aac39-542d-4803-b28e-5de9e100b9f8"]
 
 
 @pytest.mark.sandboxtest
-@pytest.mark.parametrize("ods_code", VALID_ODS_CODES)
+@pytest.mark.parametrize("ods_code", SINGLE_PAGE_ODS_CODES)
 @pytest.mark.parametrize("page", INVALID_PAGE_NUMBERS)
 @pytest.mark.parametrize("correlation_id", CORRELATION_IDS)
 def test_404_page_not_found(nhsd_apim_proxy_url, ods_code, page, correlation_id):
