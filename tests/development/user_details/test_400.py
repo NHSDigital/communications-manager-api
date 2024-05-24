5c693fd3edbbee82c1410282c1561036dd93ce52
terry.herbert1 <terry.herbert1@nhs.net>
2024-05-24 16:17:02 +0100

    CCM-3976:update tests

--- tests/development/user_details/test_400.py
+++ tests/development/user_details/test_400.py
@@ -28,28 +28,3 @@ def test_400_missing_ods_code(nhsd_apim_proxy_url, nhsd_apim_auth_headers, page,
         Generators.generate_missing_value_error("queryParam.ods-organisation-code"),
         correlation_id
     )
-
-
-@pytest.mark.devtest
-@pytest.mark.parametrize("ods_code", INVALID_ODS_CODES)
-@pytest.mark.parametrize("correlation_id", CORRELATION_IDS)
-@pytest.mark.nhsd_apim_authorization({"access": "application", "level": "level3"})
-def test_400_invalid_ods_code(nhsd_apim_proxy_url, nhsd_apim_auth_headers, ods_code, correlation_id):
-
-    """
-    .. include:: ../../partials/invalid_ods_code/test_400_nhs_accounts_invalid_ods_code.rst
-    """
-    resp = requests.get(f"{nhsd_apim_proxy_url}{USER_DETAILS_ENDPOINT}", headers={
-        **nhsd_apim_auth_headers,
-        "X-Correlation-Id": correlation_id,
-        "Accept": "application/vnd.api+json"
-    }, params={
-        ODS_CODE_PARAM_NAME: ods_code
-    })
-
-    Assertions.assert_error_with_optional_correlation_id(
-        resp,
-        400,
-        Generators.generate_invalid_value_error("queryParam.ods-organisation-code"),
-        correlation_id
-    )
--- tests/development/user_details/test_404.py
+++ tests/development/user_details/test_404.py
@@ -2,7 +2,7 @@ import requests
 import pytest
 from lib import Assertions, Generators
 from lib.constants.user_details_paths import USER_DETAILS_ENDPOINT, ODS_CODE_PARAM_NAME, PAGE_PARAM_NAME, \
-    LIVE_ODS_CODES, CORRELATION_IDS
+    LIVE_ODS_CODES, CORRELATION_IDS, INVALID_ODS_CODES
 
 INVALID_PAGE_NUMBERS = [1000, 2000, 3000]
 
@@ -24,6 +24,32 @@ def test_404_page_not_found(nhsd_apim_proxy_url, nhsd_apim_auth_headers, ods_cod
     }, params={
         ODS_CODE_PARAM_NAME: ods_code,
         PAGE_PARAM_NAME: page
+    })
+
+    Assertions.assert_error_with_optional_correlation_id(
+        resp,
+        404,
+        Generators.generate_not_found_error(),
+        correlation_id
+    )
+
+
+
+@pytest.mark.devtest
+@pytest.mark.parametrize("ods_code", INVALID_ODS_CODES)
+@pytest.mark.parametrize("correlation_id", CORRELATION_IDS)
+@pytest.mark.nhsd_apim_authorization({"access": "application", "level": "level3"})
+def test_400_invalid_ods_code(nhsd_apim_proxy_url, nhsd_apim_auth_headers, ods_code, correlation_id):
+
+    """
+    .. include:: ../../partials/invalid_ods_code/test_400_nhs_accounts_invalid_ods_code.rst
+    """
+    resp = requests.get(f"{nhsd_apim_proxy_url}{USER_DETAILS_ENDPOINT}", headers={
+        **nhsd_apim_auth_headers,
+        "X-Correlation-Id": correlation_id,
+        "Accept": "application/vnd.api+json"
+    }, params={
+        ODS_CODE_PARAM_NAME: ods_code
     })
 
     Assertions.assert_error_with_optional_correlation_id(
