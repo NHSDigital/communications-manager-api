7e6c2068769d681c3aa420e933447f228eb019ec
Ian Hodges <ian.hodges@bjss.com>
2024-06-03 11:16:31 +0100

    CCM-5115: add non prod credentials

--- tests/lib/authentication.py
+++ tests/lib/authentication.py
@@ -16,8 +16,8 @@ class Authentication():
         global call_time
 
         if env == "internal-dev":
-            api_key = os.environ.get("API_KEY")
-            private_key = os.environ.get("INTEGRATION_PRIVATE_KEY")
+            api_key = os.environ.get("NON_PROD_API_KEY")
+            private_key = os.environ.get("NON_PROD_PRIVATE_KEY")
             url = "https://internal-dev.api.service.nhs.uk/oauth2/token"
             kid = "local"
         elif env == "int":
