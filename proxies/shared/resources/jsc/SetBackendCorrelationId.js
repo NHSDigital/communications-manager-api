f699bee45bb8817f75cba07da67e22e72e28027f
Tom Webb <tom.webb4@nhs.net>
2024-06-04 13:32:01 +0100

    CCM-4963: update docs and remove ref to messageID as default correlationID

--- proxies/shared/resources/jsc/SetBackendCorrelationId.js
+++ proxies/shared/resources/jsc/SetBackendCorrelationId.js
@@ -2,10 +2,7 @@ const incomingCorrelationId = context.getVariable("request.header.x-correlation-
 const messageId = context.getVariable("messageid");
 
 if (incomingCorrelationId && messageId) {
-    context.setVariable("backendCorrelationId", incomingCorrelationId + "." + messageId);
-}
-else if (incomingCorrelationId && !messageId) {
-  context.setVariable("backendCorrelationId", incomingCorrelationId);
+    context.setVariable("backendCorrelationId", incomingCorrelationId);
 }
 else {
     context.setVariable("backendCorrelationId", messageId);
