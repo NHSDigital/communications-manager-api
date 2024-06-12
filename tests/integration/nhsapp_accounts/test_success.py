1038488379a3ae6f644eea9dbd6b0c45d169e8cf
Rachel Kennedy <rachel.kennedy13@nhs.net>
2024-06-12 14:41:38 +0100

    CCM-5461: Remove multiple page tests from internal-qa and int

--- tests/integration/nhsapp_accounts/test_success.py
+++ tests/integration/nhsapp_accounts/test_success.py
@@ -3,8 +3,7 @@ import pytest
 from lib import Assertions, Generators, Authentication
 from lib.constants.constants import INT_URL
 from lib.constants.nhsapp_accounts_paths import NHSAPP_ACCOUNTS_ENDPOINT, ODS_CODE_PARAM_NAME, PAGE_PARAM_NAME, \
-    LIVE_ODS_CODES, CORRELATION_IDS, VALID_MULTI_PAGE_NUMBERS, \
-    MULTI_LAST_PAGE, VALID_SINGLE_PAGE_NUMBERS
+    LIVE_ODS_CODES, CORRELATION_IDS, VALID_SINGLE_PAGE_NUMBERS
 
 
 @pytest.mark.inttest
@@ -26,26 +25,3 @@ def test_single_page(ods_code, page, correlation_id):
     })
 
     Assertions.assert_200_response_nhsapp_accounts(resp, INT_URL, ods_code, 1)
-
-
-@pytest.mark.inttest
-@pytest.mark.parametrize("ods_code", LIVE_ODS_CODES)
-@pytest.mark.parametrize("page", VALID_MULTI_PAGE_NUMBERS)
-@pytest.mark.parametrize("correlation_id", CORRELATION_IDS)
-def test_multi_pages(ods_code, page, correlation_id):
-
-    """
-    .. include:: ../../partials/happy_path/test_200_get_nhsapp_accounts_multi_pages.rst
-    """
-    resp = requests.get(f"{INT_URL}{NHSAPP_ACCOUNTS_ENDPOINT}", headers={
-        "Authorization": Authentication.generate_authentication("int"),
-        "X-Correlation-Id": correlation_id,
-        "Accept": "application/vnd.api+json"
-    }, params={
-        ODS_CODE_PARAM_NAME: ods_code,
-        PAGE_PARAM_NAME: page
-    })
-
-    self_page_in_response = page if page is not None else 1
-
-    Assertions.assert_200_response_nhsapp_accounts(resp, INT_URL, ods_code, self_page_in_response)
