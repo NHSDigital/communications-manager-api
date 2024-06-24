c099b7346b000414e3b04a8249d95eaa796fb05f
Ada Smaldon <ada.smaldon@bjss.com>
2024-06-24 15:55:05 +0100

    CCM-5115: further updates to newly added tests

--- tests/development/nhsapp_accounts/test_400.py
+++ tests/development/nhsapp_accounts/test_400.py
@@ -4,19 +4,19 @@ from lib import Assertions, Generators
 import lib.constants.constants as constants
 from lib.constants.nhsapp_accounts_paths import NHSAPP_ACCOUNTS_ENDPOINT, ODS_CODE_PARAM_NAME, PAGE_PARAM_NAME, \
     VALID_MULTI_PAGE_NUMBERS, INVALID_ODS_CODES, CORRELATION_IDS, LIVE_ODS_CODES, INVALID_PAGES
+from lib.fixtures import *
 
 
 @pytest.mark.devtest
 @pytest.mark.parametrize("page", VALID_MULTI_PAGE_NUMBERS)
 @pytest.mark.parametrize("correlation_id", CORRELATION_IDS)
-@pytest.mark.nhsd_apim_authorization({"access": "application", "level": "level3"})
-def test_400_missing_ods_code(nhsd_apim_proxy_url, nhsd_apim_auth_headers, page, correlation_id):
+def test_400_missing_ods_code(nhsd_apim_proxy_url, bearer_token_internal_dev, page, correlation_id):
 
     """
     .. include:: ../../partials/invalid_ods_code/test_400_nhsapp_accounts_missing_ods_code.rst
     """
     resp = requests.get(f"{nhsd_apim_proxy_url}{NHSAPP_ACCOUNTS_ENDPOINT}", headers={
-        **nhsd_apim_auth_headers,
+        "Authorization": bearer_token_internal_dev,
         "X-Correlation-Id": correlation_id,
         "Accept": "application/vnd.api+json"
     }, params={
@@ -34,14 +34,13 @@ def test_400_missing_ods_code(nhsd_apim_proxy_url, nhsd_apim_auth_headers, page,
 @pytest.mark.devtest
 @pytest.mark.parametrize("ods_code", INVALID_ODS_CODES)
 @pytest.mark.parametrize("correlation_id", CORRELATION_IDS)
-@pytest.mark.nhsd_apim_authorization({"access": "application", "level": "level3"})
-def test_400_invalid_ods_code(nhsd_apim_proxy_url, nhsd_apim_auth_headers, ods_code, correlation_id):
+def test_400_invalid_ods_code(nhsd_apim_proxy_url, bearer_token_internal_dev, ods_code, correlation_id):
 
     """
     .. include:: ../../partials/invalid_ods_code/test_400_nhsapp_accounts_invalid_ods_code.rst
     """
     resp = requests.get(f"{nhsd_apim_proxy_url}{NHSAPP_ACCOUNTS_ENDPOINT}", headers={
-        **nhsd_apim_auth_headers,
+        "Authorization": bearer_token_internal_dev,
         "X-Correlation-Id": correlation_id,
         "Accept": "application/vnd.api+json"
     }, params={
@@ -60,14 +59,13 @@ def test_400_invalid_ods_code(nhsd_apim_proxy_url, nhsd_apim_auth_headers, ods_c
 @pytest.mark.parametrize("ods_code", LIVE_ODS_CODES)
 @pytest.mark.parametrize("page", INVALID_PAGES)
 @pytest.mark.parametrize("correlation_id", CORRELATION_IDS)
-@pytest.mark.nhsd_apim_authorization({"access": "application", "level": "level3"})
-def test_400_invalid_page(nhsd_apim_proxy_url, nhsd_apim_auth_headers, ods_code, page, correlation_id):
+def test_400_invalid_page(nhsd_apim_proxy_url, bearer_token_internal_dev, ods_code, page, correlation_id):
 
     """
     .. include:: ../../partials/invalid_page/test_400_nhsapp_accounts_invalid_page.rst
     """
     resp = requests.get(f"{nhsd_apim_proxy_url}{NHSAPP_ACCOUNTS_ENDPOINT}", headers={
-        **nhsd_apim_auth_headers,
+        "Authorization": bearer_token_internal_dev,
         "X-Correlation-Id": correlation_id,
         "Accept": "application/vnd.api+json"
     }, params={
