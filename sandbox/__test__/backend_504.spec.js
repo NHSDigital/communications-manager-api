e8f20889d067855191211765b1c48a04d7673637
Simon Labarere <simon.labarere1@nhs.net>
2024-06-25 14:35:06 +0100

    CCM-4969: Update sandbox mock to return X-Correlation-Id header

--- sandbox/__test__/backend_504.spec.js
+++ sandbox/__test__/backend_504.spec.js
@@ -1,5 +1,6 @@
 import request from "supertest"
 import { setup } from './helpers.js'
+import * as uuid from 'uuid';
 
 describe('backend_504', () => {
   let env;
@@ -21,4 +22,12 @@ describe('backend_504', () => {
       .expect(504, '504 Gateway Timeout', done);
   });
 
+  it("returns a X-Correlation-Id when provided", (done) => {
+    const correlation_id = uuid.v4();
+    request(server)
+      .get('/_timeout_504')
+      .set('X-Correlation-Id', correlation_id)
+      .expect(504, '504 Gateway Timeout')
+      .expect("X-Correlation-Id", correlation_id, done);
+  });
 })
