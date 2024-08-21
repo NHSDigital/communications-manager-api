843d1888394996f85a83cb54839b5883df0edc54
Aaron Downing <aaron.downing@bjss.com>
2024-08-21 14:54:01 +0100

    CCM-6020: generate new token if current will expire in 15s

--- tests/lib/authentication.py
+++ tests/lib/authentication.py
@@ -31,19 +31,22 @@ class AuthenticationCache():
         else:
             raise ValueError("Unknown value: ", env)
 
-        _, timestamp_of_last_token_fetch = self.tokens.get(env, (None, 0))
+        _, latest_token_expiry = self.tokens.get(env, (None, 0))
 
-        if env not in self.tokens or timestamp_of_last_token_fetch + 585 < int(time()):
+        # Generate new token if latest token will expire in 15 seconds
+        if env not in self.tokens or latest_token_expiry - 15 < int(time()):
             pk_pem = None
             with open(private_key, "r") as f:
                 pk_pem = f.read()
+
+            token_expiry = int(time()) + 180
 
             claims = {
                 "sub": api_key,
                 "iss": api_key,
                 "jti": str(uuid.uuid4()),
                 "aud": url,
-                "exp": int(time()) + 180,
+                "exp": token_expiry,
             }
             additional_headers = {"kid": kid}
 
@@ -61,7 +64,7 @@ class AuthenticationCache():
             )
             details = json.loads(resp.content)
 
-            self.tokens[env] = (f"Bearer {details.get('access_token')}", int(time()))
+            self.tokens[env] = (f"Bearer {details.get('access_token')}", token_expiry)
 
         bearer_token = self.tokens[env][0]
         return Secret(bearer_token)
