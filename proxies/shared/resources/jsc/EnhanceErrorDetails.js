ff8251c849b1ade4260569651eab8524b39d1d45
Ian Hodges <ian.hodges@bjss.com>
2024-09-17 14:10:40 +0100

    CCM-6471: simplify error translation

--- proxies/shared/resources/jsc/EnhanceErrorDetails.js
+++ proxies/shared/resources/jsc/EnhanceErrorDetails.js
@@ -7,19 +7,10 @@ const links = {
 
 const enhancedErrors = [];
 
-const errorCodes = {
-    'Property cannot be null': 'CM_NULL_VALUE',
-    'Duplicate value': 'CM_DUPLICATE_VALUE',
-    'Missing value': 'CM_MISSING_VALUE',
-    'Too few items': 'CM_TOO_FEW_ITEMS'
-};
-
 JSON.parse(errors).forEach((error, index) => {
-    const code = errorCodes[error.title] || 'CM_INVALID_VALUE';
-
     enhancedErrors.push({
         id: messageId + '.' + index,
-        code: code,
+        code: error.code,
         links: links,
         status: String(statusCode),
         title: error.title,
