f8863d5cf526b3a6ba81ed3c133ce85e1643d0c2
Aaron Downing <aaron.downing@bjss.com>
2024-09-18 16:23:12 +0100

    CCM-6418: update deps

--- scripts/linter/base.js
+++ scripts/linter/base.js
@@ -1,49 +1,27 @@
-const {
-  rules: baseBestPracticesRules,
-} = require('eslint-config-airbnb-base/rules/best-practices');
-
 module.exports = {
   root: true,
+  parser: "@babel/eslint-parser",
+  parserOptions: {
+    requireConfigFile: false,
+  },
   env: {
     browser: true,
     jest: true,
     node: true,
   },
-  parser: '@typescript-eslint/parser',
   extends: [
     'airbnb',
+    'airbnb-base',
     'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:import/typescript',
-    'plugin:react/recommended',
     'plugin:security/recommended',
     'prettier',
   ],
   plugins: [
-    '@typescript-eslint',
     'html',
     'import',
-    'react',
-    'react-hooks',
     'security',
   ],
-  globals: {
-    Atomics: 'readonly',
-    React: 'writable',
-    SharedArrayBuffer: 'readonly',
-  },
   rules: {
-    '@typescript-eslint/no-explicit-any': 'error',
-    '@typescript-eslint/no-non-null-assertion': 0,
-    '@typescript-eslint/no-shadow': 'error',
-    '@typescript-eslint/no-unused-vars': [
-      'error',
-      {
-        ...baseBestPracticesRules['no-unused-vars'],
-        ignoreRestSiblings: true,
-        argsIgnorePattern: '^_',
-      },
-    ],
     camelcase: 'warn',
     'class-methods-use-this': 0,
     'comma-dangle': ['error', 'only-multiline'],
@@ -94,30 +72,10 @@ module.exports = {
     'no-underscore-dangle': 0,
     'no-use-before-define': 0,
     radix: 0,
-    'react-hooks/exhaustive-deps': 'error',
-    'react-hooks/rules-of-hooks': 'error',
-    'react/function-component-definition': 0,
-    'react/jsx-filename-extension': 0,
-    'react/jsx-no-useless-fragment': ['error', { allowExpressions: true }],
-    'react/jsx-props-no-spreading': 0,
-    'react/react-in-jsx-scope': 'off',
     'security/detect-object-injection': 0,
-    '@next/next/no-img-element': 'off',
     'import/extensions': 0,
   },
   overrides: [
-    {
-      // see: https://github.com/B2o5T/graphql-eslint  // TODO: go through the readme an implment graphql eslint properly
-      files: ['*.graphql'],
-      parser: '@graphql-eslint/eslint-plugin',
-      plugins: ['@graphql-eslint'],
-      rules: {
-        '@graphql-eslint/known-type-names': 'error',
-      },
-      parserOptions: {
-        schema: '**/*.graphql',
-      },
-    },
     {
       files: ['sandbox/**'],
       rules: {
@@ -130,42 +88,10 @@ module.exports = {
         'no-console': 'off',
       },
     },
-    {
-      files: ['test/integration/**'],
-      rules: {
-        'import/no-extraneous-dependencies': [
-          'error',
-          { optionalDependencies: false, bundledDependencies: false },
-        ],
-      },
-    },
-    {
-      files: ['*.js'],
-      rules: {
-        '@typescript-eslint/explicit-module-boundary-types': 0,
-        '@typescript-eslint/no-var-requires': 'off',
-        '@typescript-eslint/no-empty-function':
-          baseBestPracticesRules['no-empty-function'],
-      },
-    },
-    {
-      files: ['*.test.*'],
-      rules: {
-        '@typescript-eslint/no-explicit-any': 'off',
-      },
-    },
-    {
-      files: ['*.tsx'],
-      rules: {
-        'react/require-default-props': 0,
-      },
-    },
   ],
   ignorePatterns: [
-    '.next',
     'target',
     'jest.config.js',
     'chd_api_types.ts',
-    '**/data_layer/**/*.ts',
   ],
 };
