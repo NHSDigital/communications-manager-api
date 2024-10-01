fd0cad9681ef8624086fd371fc706571b02eab37
Ada Smaldon <ada.smaldon@bjss.com>
2024-10-01 17:00:29 +0100

    CCM-6126: remove backend test for sandbox environment - not applicable

--- tests/sandbox/message_batches/create_message_batches/test_too_large.py
+++ tests/sandbox/message_batches/create_message_batches/test_too_large.py
@@ -38,38 +38,3 @@ def test_too_many_messages(nhsd_apim_proxy_url):
 
     Assertions.assert_error_with_optional_correlation_id(resp, 413, None, None)
     assert len(resp.json()["errors"]) == 1
-
-
-@pytest.mark.sandboxtest
-def test_payload_too_large(nhsd_apim_proxy_url):
-    """
-    .. include:: ../../partials/too_large/test_too_many_messages.rst
-    """
-
-    data = Generators.generate_valid_create_message_batch_body("sandbox")
-    valid_recipient = data["data"]["attributes"]["messages"][0]["recipient"]
-    large_personalisation = {'body': 'x'*32}
-    messages = []
-    data["data"]["attributes"]["messages"] = messages
-
-    for i in range(MESSAGE_LIMIT):
-        messages.append({
-            "messageReference": str(uuid.uuid1()),
-            "recipient": valid_recipient,
-            "personalisation": large_personalisation
-            })
-    # make sure it's more than 6MB to be a fair test
-    payload_length = len(json.dumps(data))
-    assert payload_length > 6*(1024**2)
-    # but less than 10MB to make sure it doesn't fail because apigee didn't accept it
-    assert payload_length < 10000000
-
-    resp = requests.post(f"{nhsd_apim_proxy_url}{MESSAGE_BATCHES_ENDPOINT}", headers={
-            "Accept": CONTENT_TYPE,
-            "Content-Type": CONTENT_TYPE
-        },
-        json=data
-    )
-
-    Assertions.assert_error_with_optional_correlation_id(resp, 413, None, None)
-    assert len(resp.json()["errors"]) == 1
