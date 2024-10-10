3ba668dce3bc77cfabf47325280e412ad8f41986
Simon Labarere <simon.labarere1@nhs.net>
2024-10-10 13:46:13 +0100

    CCM-6648: Fix intermittent 401 errors

--- tests/lib/authentication.py
+++ tests/lib/authentication.py
@@ -11,28 +11,50 @@ class AuthenticationCache():
     def __init__(self):
         self.tokens = {}
 
+        # Number of consecutive authentication tests before considering it is successful.
+        # This is required because apigee might be using load balancing.
+        self.consecutive_tests = 4
+
+        # Number of seconds before trying a test again
+        self.time_between_tests = 10
+
+        # Number of attempts before giving up. It can take up to 5 minutes for the
+        # addition of a product to an application to take effect.
+        # time_between_tests * max_tests = 300 seconds = 5 minutes.
+        self.max_tests = 30
+
+        # How long the token will stay valid
+        self.token_validity = 180
+
     def generate_authentication(self, env):
 
+        # For the test_url, note that we don't need a message_id that actually exists in
+        # the backend. The test will only check that the API doesn't return a 401,
+        # a 404 response means the authentication is working.
         if env == "internal-dev":
             api_key = os.environ["NON_PROD_API_KEY"]
             private_key = os.environ["NON_PROD_PRIVATE_KEY"]
             url = "https://internal-dev.api.service.nhs.uk/oauth2/token"
             kid = "local"
+            test_url = "https://internal-dev.api.service.nhs.uk/comms/v1/messages/message_id"
         elif env == "internal-dev-test-1":
             api_key = os.environ["NON_PROD_API_KEY_TEST_1"]
             private_key = os.environ["NON_PROD_PRIVATE_KEY"]
             url = "https://internal-dev.api.service.nhs.uk/oauth2/token"
             kid = "local"
+            test_url = "https://internal-dev.api.service.nhs.uk/comms/v1/messages/message_id"
         elif env == "int":
             api_key = os.environ.get("INTEGRATION_API_KEY")
             private_key = os.environ.get("INTEGRATION_PRIVATE_KEY")
             url = "https://int.api.service.nhs.uk/oauth2/token"
             kid = "local"
+            test_url = "https://int.api.service.nhs.uk/comms/v1/messages/message_id"
         elif env == "prod":
             api_key = os.environ.get("PRODUCTION_API_KEY")
             private_key = os.environ.get("PRODUCTION_PRIVATE_KEY")
             url = "https://api.service.nhs.uk/oauth2/token"
             kid = "prod-1"
+            test_url = "https://api.service.nhs.uk/comms/v1/messages/message_id"
         else:
             raise ValueError("Unknown value: ", env)
 
@@ -40,36 +62,82 @@ class AuthenticationCache():
 
         # Generate new token if latest token will expire in 15 seconds
         if env not in self.tokens or latest_token_expiry < int(time()) + 15:
-            pk_pem = None
-            with open(private_key, "r") as f:
-                pk_pem = f.read()
-
-            token_expiry = int(time()) + 180
-
-            claims = {
-                "sub": api_key,
-                "iss": api_key,
-                "jti": str(uuid.uuid4()),
-                "aud": url,
-                "exp": token_expiry,
-            }
-            additional_headers = {"kid": kid}
-
-            j = jwt.encode(
-                claims, pk_pem, algorithm="RS512", headers=additional_headers
-            )
-
-            resp = requests.post(url, headers={
-                "Content-Type": "application/x-www-form-urlencoded"
-            }, data={
-                "grant_type": "client_credentials",
-                "client_assertion_type": "urn:ietf:params:oauth:client-assertion-type:jwt-bearer",
-                "client_assertion": j
-            }
-            )
-            details = json.loads(resp.content)
-
-            self.tokens[env] = (f"Bearer {details.get('access_token')}", token_expiry)
+            self.tokens[env] = self.generate_and_test_new_token(api_key, private_key, url, kid, test_url)
 
         bearer_token = self.tokens[env][0]
         return Secret(bearer_token)
+
+    def generate_and_test_new_token(self, api_key, private_key, url, kid, test_url):
+        new_token = None
+        valid_auth = False
+        time_since_new_token = int(time())
+
+        for i in range(self.max_tests):
+            print(f"Testing new token, attemp #{i+1}")
+            if new_token is None:
+                new_token = self.generate_new_token(api_key, private_key, url, kid)
+
+            if self.test_token(test_url, new_token[0]):
+                valid_auth = True
+                break
+
+            # The test fail, give apigee some time to update its cache.
+            time.sleep(self.time_between_tests)
+
+            if int(time()) - time_since_new_token > (self.token_validity / 2):
+                # Token about to expire, generate a new one
+                new_token = None
+
+        if valid_auth:
+            print("Token generated successfully")
+            return new_token
+
+        print("Could not generate token")
+        raise Exception("Could not generate token")
+
+    def generate_new_token(self, api_key, private_key, url, kid):
+        pk_pem = None
+        with open(private_key, "r") as f:
+            pk_pem = f.read()
+
+        token_expiry = int(time()) + self.token_validity
+
+        claims = {
+            "sub": api_key,
+            "iss": api_key,
+            "jti": str(uuid.uuid4()),
+            "aud": url,
+            "exp": token_expiry,
+        }
+        additional_headers = {"kid": kid}
+
+        j = jwt.encode(
+            claims, pk_pem, algorithm="RS512", headers=additional_headers
+        )
+
+        resp = requests.post(url, headers={
+            "Content-Type": "application/x-www-form-urlencoded"
+        }, data={
+            "grant_type": "client_credentials",
+            "client_assertion_type": "urn:ietf:params:oauth:client-assertion-type:jwt-bearer",
+            "client_assertion": j
+        }
+        )
+        details = json.loads(resp.content)
+
+        return (f"Bearer {details.get('access_token')}", token_expiry)
+
+    def test_token(self, test_url, token):
+        for _ in range(self.consecutive_tests):
+            resp = requests.get(
+                test_url,
+                headers={
+                    "Authorization": token,
+                    "Accept": "*/*",
+                    "Content-Type": "application/json"
+                },
+            )
+            if resp.status_code == 401:
+                return False
+
+        return True
