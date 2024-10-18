a3675f25a7d007cc485697b4b335edf8b3a76857
Ian Hodges <ian.hodges1@nhs.net>
2024-10-18 09:34:15 +0100

    CCM-6434: Respond with request item IDs for batch create (#759)

--- tests/development/content_types/test_missing_accept_header.py
+++ tests/development/content_types/test_missing_accept_header.py
@@ -34,6 +34,4 @@ def test_missing_accept_header(
 
     error_handler.handle_retry(resp)
 
-    Assertions.assert_201_response(
-        resp, data["data"]["attributes"]["messageBatchReference"], data["data"]["attributes"]["routingPlanId"]
-    )
+    Assertions.assert_201_response(resp, data)
