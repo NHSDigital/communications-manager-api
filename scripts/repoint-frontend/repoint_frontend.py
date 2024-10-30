a298fb35915a584e32b6d7ad235c7536177f2c4f
lapenna-bjss <luca.lapenna@bjss.com>
2024-10-30 11:51:05 +0000

    CCM-5694: refactor repoint_frontend.py

--- scripts/repoint-frontend/repoint_frontend.py
+++ scripts/repoint-frontend/repoint_frontend.py
@@ -2,16 +2,7 @@ import argparse
 import os
 import sys
 
-remove_lines = [
-    "<LoadBalancer>",
-    "<Server name=\"{{ TARGET_SERVER_OVERRIDE | default(\'communications-manager-target\') }}\"/>",
-    "</LoadBalancer>",
-    "<Path>{requestpath}</Path>",
-]
-
-replacement_line = "        <URL>https://comms-apim.{environment}.communications.national.nhs.uk</URL>\n"
-
-configs = [
+default_config = [
     (
         "proxies/shared/policies/AssignMessage.MessageBatches.Create.Request.xml",
         "/api/v1/send",
@@ -38,8 +29,11 @@ configs = [
     ),
 ]
 
+alternate_config = [
+    ("proxies/live/apiproxy/targets/target.xml", "{environment}"),
+]
 
-# Helper function to check file existence
+
 def read_file(file_path):
     if not os.path.exists(file_path):
         print(f"Error: The file at {file_path} does not exist.")
@@ -53,33 +47,7 @@ def write_file(file_path, lines):
         file.writelines(lines)
 
 
-def modify_target_file(environment, file_path):
-    replaced_line = replacement_line.format(environment=environment)
-    lines = read_file(file_path)
-
-    new_lines = []
-    skip = False
-
-    for line in lines:
-        # if the line matches the start of the block we want to remove
-        if line.strip() == remove_lines[0].strip():
-            skip = True
-        # if we are skipping lines, and we reach the end of the block
-        elif skip and line.strip() == remove_lines[-1].strip():
-            skip = False
-            # Instead of adding the removed lines, add the replacement line
-            new_lines.append(replaced_line)
-        # if we are not skipping, add the line to the new content
-        elif not skip:
-            new_lines.append(line)
-
-    write_file(file_path, new_lines)
-
-    print(f"File at {file_path} successfully updated with environment '{environment}'.")
-
-
-def get_config(path, endpoint, url, template_tag, environment):
-    file_path = path
+def get_config_default(path, endpoint, url, template_tag, environment):
     tag_type = "Template" if template_tag else "Value"
     match_block = [
         "<AssignVariable>",
@@ -87,38 +55,76 @@ def get_config(path, endpoint, url, template_tag, environment):
         f"<{tag_type}>{endpoint}</{tag_type}>",
         "</AssignVariable>",
     ]
-
     formatted_url = url.format(environment=environment)
     insertion_lines = [
-        "    {% if ENVIRONMENT_TYPE != 'sandbox' %}",
+        "    {% if ENVIRONMENT_TYPE == 'sandbox' %}",
+        "       <AssignVariable>",
+        "           <Name>requestpath</Name>",
+        f"           <{tag_type}>{endpoint}</{tag_type}>",
+        "       </AssignVariable>",
+        "    {% else %}",
         "        <AssignVariable>",
         "            <Name>target.url</Name>",
         f"            <Value>{formatted_url}</Value>",
         "        </AssignVariable>",
         "    {% endif %}",
     ]
-    return file_path, match_block, insertion_lines
+    return path, match_block, insertion_lines
 
 
-def add_lines_after_block(environment, file_path, match_block, insertion_lines):
+def get_config_alternate(path, environment):
+    match_block = [
+        "<LoadBalancer>",
+        "<Server name=\"{{ TARGET_SERVER_OVERRIDE | default('communications-manager-target') }}\"/>",
+        "</LoadBalancer>",
+        "<Path>{requestpath}</Path>",
+    ]
+    insertion_lines = [
+        "       {% if ENVIRONMENT_TYPE == 'sandbox' %}",
+        "           <LoadBalancer>",
+        "	            <Server name=\"{{ TARGET_SERVER_OVERRIDE | default('communications-manager-target') }}\"/>",
+        "           </LoadBalancer>",
+        "           <Path>{requestpath}</Path>",
+        "       {% else %}",
+        f"           <URL>https://comms-apim.{environment}.communications.national.nhs.uk</URL>",
+        "       {% endif %}",
+    ]
+    return path, match_block, insertion_lines
+
+
+def get_config_by_type(block_type, *args):
+    if block_type == "default":
+        return get_config_default(*args)
+    elif block_type == "alternate":
+        return get_config_alternate(*args)
+    else:
+        raise ValueError(f"Unknown block type: {block_type}")
+
+
+def update_file(environment, file_path, match_block, insertion_lines):
     lines = read_file(file_path)
     match_block = [line.strip() for line in match_block]
 
-    # Format lines to add with environment where necessary
-    insertion_lines = [line.replace("{environment}", environment) for line in insertion_lines]
+    insertion_lines = [
+        line.replace("{environment}", environment) for line in insertion_lines
+    ]
 
     block_found, new_lines = False, []
     i = 0
+
     while i < len(lines):
+        # Check if current line matches the start of match_block
         if lines[i].strip() == match_block[0]:
+            # Check if the full match_block matches line-by-line
             match = all(
-                lines[i + j].strip() == match_block[j]
-                for j in range(len(match_block))
+                lines[i + j].strip() == match_block[j] for j in range(len(match_block))
             )
             if match:
                 block_found = True
                 i += len(match_block)
-                new_lines.extend(f"{line}\n" for line in insertion_lines)
+                new_lines.extend(
+                    f"{line}\n" for line in insertion_lines
+                )  # Insert new lines
                 continue
         new_lines.append(lines[i])
         i += 1
@@ -138,19 +144,23 @@ if __name__ == "__main__":
         sys.exit(1)
 
     parser = argparse.ArgumentParser(
-        description="Script to modify target.xml based on the environment argument."
+        description="Script to update proxy configuration files based on the environment argument."
     )
     parser.add_argument(
         "environment", type=str, help="Environment identifier (e.g. 'de-gith1')"
     )
     args = parser.parse_args()
 
-    # Modify target.xml file
-    modify_target_file(args.environment, "proxies/live/apiproxy/targets/target.xml")
-
-    # Process each config entry
-    for path, endpoint, url, template_tag in configs:
-        file_path, match_block, insertion_lines = get_config(
-            path, endpoint, url, template_tag, args.environment
+    # Process `default` block type configs
+    for path, endpoint, url, template_tag in default_config:
+        file_path, match_block, insertion_lines = get_config_by_type(
+            "default", path, endpoint, url, template_tag, args.environment
         )
-        add_lines_after_block(args.environment, file_path, match_block, insertion_lines)
+        update_file(args.environment, file_path, match_block, insertion_lines)
+
+    # Process `alternate` block type configs
+    for path, environment in alternate_config:
+        file_path, match_block, insertion_lines = get_config_by_type(
+            "alternate", path, args.environment
+        )
+        update_file(args.environment, file_path, match_block, insertion_lines)
