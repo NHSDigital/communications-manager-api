a9c2a728c5f3dcee493682e3248fd77f1e429fd3
skaram-nhs <sharon.karam1@nhs.net>
2025-01-29 12:11:19 +0000

    CCM-6135: Rename postman collections (#870)
    * CCM-6135: Rename postman collections
    
    * CCM-6135: Update scripts with new file names

--- scripts/sync_postman_collections.sh
+++ scripts/sync_postman_collections.sh
@@ -3,13 +3,13 @@
 # copy the integration collection
 echo " "
 echo "Downloading integration collection...."
-curl -s -X GET "https://api.postman.com/collections/28740466-fbe32763-302e-4a0b-b6e9-3a20f1bde923?access_key=${INTEGRATION_COLLECTION_API_KEY}" | json_pp > postman/CommunicationsManager.Integration.postman_collection.json
+curl -s -X GET "https://api.postman.com/collections/41628342-9c132bcd-f76f-448f-a58c-ba38c5d64d4f?access_key=${INTEGRATION_COLLECTION_API_KEY}" | json_pp > postman/NhsNotify.Integration.postman_collection.json
 echo "done!"
 
 # copy the sandbox collection
 echo " "
 echo "Downloading sandbox collection...."
-curl -s -X GET "https://api.postman.com/collections/28740466-ec078d1e-d4d7-4460-92b9-7d79d51f967a?access_key=${SANDBOX_COLLECTION_API_KEY}" | json_pp > postman/CommunicationsManager.Sandbox.postman_collection.json
+curl -s -X GET "https://api.postman.com/collections/41628342-33dcc9d0-8ccc-4756-8ccb-237fd78337f5?access_key=${SANDBOX_COLLECTION_API_KEY}" | json_pp > postman/NhsNotify.Sandbox.postman_collection.json
 echo "done!"
 
 # ensure we notify that the environments need to be exported manually
